028689ffdae05a602e30224d8c86a32966d6f0b3
hromadka <hromadka@gmail.com>
2021-01-26 13:24:18 -0800

    rust scaffolding

---
+++ rust/Shuffle/target/debug/Shuffle.d
@@ -0,0 +1,1 @@
+C:\Users\Ted\workspace\shuffle\rust\Shuffle\target\debug\Shuffle.exe: C:\Users\Ted\workspace\shuffle\rust\Shuffle\src\main.rs
---
+++ rust/Shuffle/target/debug/deps/Shuffle.d
@@ -0,0 +1,5 @@
+C:\Users\Ted\workspace\shuffle\rust\Shuffle\target\debug\deps\shuffle.exe: src\main.rs
+
+C:\Users\Ted\workspace\shuffle\rust\Shuffle\target\debug\deps\shuffle.d: src\main.rs
+
+src\main.rs:
